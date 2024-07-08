4fa5bfcb8e8a0acafb91fff2a9770b293b039ee4
uchihaghost12 <ajinvs21@gmail.com>
2024-07-08 14:00:10 +0530

    kiiii

---
+++ TeamCoach/TeamMembers/teamMembersSchema.js
@@ -0,0 +1,48 @@
+const mongoose=require("mongoose");
+
+const tmSchema=mongoose.Schema({
+    name:{
+        type:String,
+       
+        required:true,
+       
+    },
+    contact:{
+        type:String,
+        
+        required:true,
+       
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true,
+       
+        dropDups: true
+    },
+    pincode:{
+        type:String,
+        required:true
+    },
+    country:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true,
+    },
+    city:{
+        type:String,
+        required:true,
+    },
+    state:{
+        type:String,
+        required:true
+    }, 
+});
+module.exports=mongoose.model('teammembers',tmSchema)
